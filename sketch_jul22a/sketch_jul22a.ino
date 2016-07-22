12be51e8beee9873982a692dce7cd8585926709a
nikitarub <nikita.rub@yandex.ru>
2016-07-22 15:45:51 +0300

    Обновлен код Adruino

--- sketch_jul22a/sketch_jul22a.ino
+++ sketch_jul22a/sketch_jul22a.ino
@@ -3,10 +3,13 @@
 #define TRESHOLD            500 // Устанавливаем порог србатывания
 
 
+const int ledPin = 13;
+
 void setup() {
   // put your setup code here, to run once:
 
-pinMode(LED_PIN, OUTPUT);
+//pinMode(LED_PIN, OUTPUT);
+pinMode(ledPin, OUTPUT);
 Serial.begin(9600);
 
 }
@@ -14,17 +17,47 @@ Serial.begin(9600);
 void loop() {
   // put your main code here, to run repeatedly:
 
+
+Serial.println("Hello Pi");
+  if (Serial.available())
+  {
+     flash(Serial.read() - '0');
+  }
+  delay(1000);
+
+
+
+
+
+
  // Считаем текущее значение громкости
   int loudness = analogRead(LOUDNESS_SENSOR_PIN);
 
-  Serial.println(loudness);
- delay (100);
+  //Serial.println(loudness);
+ //delay (100);
   // Если громкость выше пороговой..
   if (loudness > TRESHOLD)
     // зажгём светодиод,
-    digitalWrite(LED_PIN, HIGH);
+    digitalWrite(ledPin, HIGH);
   else
     // а если нет - погасим
-    digitalWrite(LED_PIN, LOW); 
+    digitalWrite(ledPin, LOW); 
 
 }
+
+
+
+
+
+void flash(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+    digitalWrite(ledPin, LOW);
+    delay(100);
+  }
+}
+
+
